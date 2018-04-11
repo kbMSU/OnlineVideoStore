7e48fafaa2a0d11dafcaff8aeaef0c1c556d05c0
Karthik <KarBalasubramanian@gmail.com>
2018-04-11 13:53:05 +1000

    Add Review page setup complete
    Now navigating to the add review page.

---
+++ VideoStore.WebClient/ViewModels/ReviewViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using VideoStore.Services.Interfaces;
+
+namespace VideoStore.WebClient.ViewModels
+{
+    public class ReviewViewModel
+    {
+        private ICatalogueService CatalogueService
+        {
+            get
+            {
+                return ServiceFactory.Instance.CatalogueService;
+            }
+        }
+
+        public ReviewViewModel(int mediaId)
+        {
+            this.mediaId = mediaId;
+        }
+
+        private int mediaId;
+    }
+}
